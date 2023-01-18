acece1c7264499571f82c48ef490d10878f094e4
pks7528 <pks7528@naver.com>
2023-01-18 17:16:28 +0900

    2023-01-18

--- 07_Exception/src/ex01_try_catch/MainClass.java
+++ 07_Exception/src/ex01_try_catch/MainClass.java
@@ -7,9 +7,7 @@ public class MainClass {
 	public static void ex01() {
 		
 		try {
-			
 			String[] season = new String[4];
-			
 			season[0] = "spring";
 			season[1] = "summer";
 			season[2] = "autumn";
@@ -23,7 +21,6 @@ public class MainClass {
 		} catch(Exception e) { // Unchecked Exception의 경우 RuntimeException으로도 처리 가능
 			System.out.println("ArrayIndexOutOfBoundsException 발생");
 		}
-			
 	}	
 	
 	public static void ex02() {
@@ -32,7 +29,6 @@ public class MainClass {
 			String input = "1,2,3,,4,5";
 			String[] numbers = input.split(",");  
 			int[] iNumbers = new int[numbers.length];
-			
 			
 			for(int i=0; i<numbers.length; i++) {
 				iNumbers[i] = Integer.parseInt(numbers[i]);
@@ -45,16 +41,24 @@ public class MainClass {
 	
 	public static void ex03() {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("계산식을 입력하시오(예: 1+2) >>>");
-		String expr = sc.next();
-		String[] item = expr.split("[+]");
-		int number1 = Integer.parseInt(item[0]);
-		int number2 = Integer.parseInt(item[1]);
-		System.out.println(number1 + number2);
-		sc.close();
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("계산식을 입력하시오(예: 1+2) >>>");
+			String expr = sc.next();
+			String[] item = expr.split("[+]");
+			int number1 = Integer.parseInt(item[0]);
+			int number2 = Integer.parseInt(item[1]);
+			System.out.println(number1 + number2);
+			sc.close();
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("+를 포함해서 입력하세요.");
+		} catch(NumberFormatException e) {
+			System.out.println("정수 덧셈만 가능합니다.");
+		} catch(Exception e) {									// 마지막 catch 블록은 Exception으로 모든 예외를 처리한다.
+			System.out.println("예외가 발생했습니다.");
+		}
 	}
-	
+		
 	public static void main(String[] args) {
 		ex03();
 	}
