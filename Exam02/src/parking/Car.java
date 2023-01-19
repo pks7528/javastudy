f87dca4d440d8f0b76d347ecb4d337429662e485
pks7528 <pks7528@naver.com>
2023-01-19 19:53:16 +0900

    2023-01-19

---
+++ Exam02/src/parking/Car.java
@@ -0,0 +1,22 @@
+package parking;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor			// new Car("", "")
+public class Car {
+	
+	//필드
+	private String carNo;
+	private String model;
+	
+	//생성자
+	
+	
+	//메소드
+}
