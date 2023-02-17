1569cabbe3eb50b9be99d80370da0abc4077b358
pks7528 <pks7528@naver.com>
2023-02-17 17:29:42 +0900

    2023-02-17

---
+++ Appendix04_Jdbc/src/service/ContactService.java
@@ -0,0 +1,10 @@
+package service;
+
+public interface ContactService {
+	public void addContact();
+	public void removeContact();
+	public void modifyContact();
+	public void findContactsByName();
+	public void findContactsAllByName();
+	
+}
